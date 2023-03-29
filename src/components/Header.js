3020d51dfd9ba863a9f7b71ec276f0406973ef66
Tijana Zajic <tijanazajic1204@gmail.com>
2023-03-29 19:25:49 +0200

    root min-height change

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,11 @@ export const Header = () => {
               <nav>
     <div className="nav-wrapper blue header">
         <span className='flow-text'>Interviews Reports</span>
-        <button className='blue button' onClick={()=> navigate('/')}>Candidates</button> 
+        <div>
+          <button className='blue button' onClick={()=> navigate('/')}>Candidates</button> 
+          <button className='blue button second-btn' onClick={()=> navigate()}>Reports</button> 
+        </div>
+        
     </div>
   </nav>
         
