6da765ad750434202e131382a3c3cd87d775f38e
Tijana Zajic <tijanazajic1204@gmail.com>
2023-03-29 19:36:32 +0200

    modal font-size change

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ const Card = ({candidates}) => {
         <div>
         <div className='container row' id='candidate'>
             {candidates.map(candidate => (
-                <div className='card col s6' onClick={() => navigate(`/candidate/${candidate.id}`)}>
+                <div className='card col' onClick={() => navigate(`/candidate/${candidate.id}`)}>
             <div className='card-image'>
                 <img src= {
                         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5qM2m_aEWRcBU1u-tMav3P6RBYWDIJQMVHx9IIeixwFITKDDyR43ok9TJRtoP2BeyYtk&usqp=CAU"
