7694c3bea41a13c76b3e5a4d906c837adf26212d
katarinjah <katarina.d.262@gmail.com>
2023-03-31 13:59:56 +0200

    Worked on Reports search logic

--- src/components/Reports/Reports.js
+++ src/components/Reports/Reports.js
@@ -38,54 +38,94 @@ export const Reports = ({openModal, setOpenModal}) => {
         const doiMonth = (doiDate.getMonth() + 1).toString().padStart(2, '0');
         const doiYear = doiDate.getFullYear();
         return (`${doiDay}.${doiMonth}.${doiYear}`);
-    };      
+    };   
+    
+    const searchByCompany = reports.filter(report => report.companyName.toLowerCase().includes(searchQuery.toLocaleLowerCase()));
+    const searchByCandidate = reports.filter(report => report.candidateName.toLowerCase().includes(searchQuery.toLowerCase()));
 
     return (
         <div className={`${openModal ? "modalUp" : "modalDown"}`} id='main-wrapper'>
-           <div className="row search-wrapper">
-            <div className="col s6 m4 l3 xl3">
-            <form>
-                <div className="input-field">
-                    <i className="material-icons prefix"><FaSearch className="search-icon"/></i>
-                    <input 
-                        type="search" 
-                        placeholder="Search" 
-                        value={searchQuery} 
-                        onChange={(event) => setSearchQuery(event.target.value)}
-                    />
+            <div className="row search-wrapper">
+                <div className="col s6 m4 l3 xl3">
+                    <form>
+                        <div className="input-field">
+                            <i className="material-icons prefix"><FaSearch className="search-icon"/></i>
+                            <input 
+                                type="search" 
+                                placeholder="Search" 
+                                value={searchQuery} 
+                                onChange={event => setSearchQuery(event.target.value)}
+                            />
+                        </div>
+                    </form>
                 </div>
-            </form>
             </div>
-            </div>
+           
             <div className="container reports-container">
-            {openModal && <Modal report={modalReport} setOpenModal={setOpenModal}/>} 
-                {reports && 
-                    reports.map(report => ( 
-                        <div className="row report-data" key={report.id}>
-                            <div className="col s12 m3 l3 xl3">
-                                <div className="truncate report-info">{report.companyName}</div>
-                                <div className="tiny-text">Company</div>
+                {openModal && <Modal report={modalReport} setOpenModal={setOpenModal}/>} 
+                {searchQuery && searchByCompany.length === 0 && searchByCandidate.length === 0 ? (
+                    <div className="error-message">No matching reports found</div>
+                    ) : (
+                        searchByCompany.length > 0 ? (
+                            searchByCompany.map(report => (
+                                <div className="row report-data" key={report.id}>
+                                    <div className="col s12 m3 l3 xl3">
+                                        <div className="truncate report-info">{report.companyName}</div>
+                                        <div className="tiny-text">Company</div>
+                                </div>
+                                <div className="col s12 m3 l3 xl3">
+                                    <div className="truncate report-info">{report.candidateName}</div>
+                                    <div className="tiny-text">Candidate</div>
+                                </div>
+                                <div className="col s12 m3 l3 xl3">
+                                    <div className="truncate report-info">{formatInterviewDate(report.interviewDate)}</div>
+                                    <div className="tiny-text">Interview date</div>
+                                </div>
+                                <div className="col s12 m2 l2 xl2">
+                                    <div className="truncate report-info">{report.status}</div>
+                                    <div className="tiny-text">Status</div>
+                                </div>
+                                <div className="col s12 m1 l1 xl1 valign-wrapper">
+                                    <span className="eye-icon">
+                                    <FaRegEye onClick={()=> {setOpenModal(true); setModalReport(report)}}  />
+                                    </span>
+                                    <span className="close-icon">
+                                    <FaRegWindowClose onClick={() => deleteHandler(report.id)} />
+                                    </span>
+                                </div>
                             </div>
-                            <div className="col s12 m3 l3 xl3">
-                                <div className="truncate report-info">{report.candidateName}</div>
-                                <div className="tiny-text">Candidate</div>
+                        ))
+                    ) : (
+                        searchByCandidate.map(report => (
+                            <div className="row report-data" key={report.id}>
+                                <div className="col s12 m3 l3 xl3">
+                                    <div className="truncate report-info">{report.companyName}</div>
+                                    <div className="tiny-text">Company</div>
+                                </div>
+                                <div className="col s12 m3 l3 xl3">
+                                    <div className="truncate report-info">{report.candidateName}</div>
+                                    <div className="tiny-text">Candidate</div>
+                                </div>
+                                <div className="col s12 m3 l3 xl3">
+                                    <div className="truncate report-info">{formatInterviewDate(report.interviewDate)}</div>
+                                    <div className="tiny-text">Interview date</div>
+                                </div>
+                                <div className="col s12 m2 l2 xl2">
+                                    <div className="truncate report-info">{report.status}</div>
+                                    <div className="tiny-text">Status</div>
+                                </div>
+                                <div className="col s12 m1 l1 xl1 valign-wrapper">
+                                    <span className="eye-icon">
+                                    <FaRegEye onClick={()=> {setOpenModal(true); setModalReport(report)}}  />
+                                    </span>
+                                    <span className="close-icon">
+                                    <FaRegWindowClose onClick={() => deleteHandler(report.id)} />
+                                    </span>
+                                </div>
                             </div>
-                            <div className="col s12 m3 l3 xl3">
-                                <div className="truncate report-info">{formatInterviewDate(report.interviewDate)}</div>
-                                <div className="tiny-text">Interview date</div>
-                            </div>
-                            <div className="col s12 m2 l2 xl2">
-                                <div className="truncate report-info">{report.status}</div>
-                                <div className="tiny-text">Status</div>
-                            </div>
-                            <div className="col s12 m1 l1 xl1 valign-wrapper">
-                                <span className="eye-icon">
-                                    <FaRegEye onClick={()=> {setOpenModal(true); setModalReport(report)}}  /></span>
-                                <span className="close-icon"><FaRegWindowClose onClick={() => deleteHandler(report.id)} /></span>
-                            </div>
-                        </div> 
-                    )) 
-                }      
+                        ))
+                    )
+                )}
             </div>
         </div>
     )
