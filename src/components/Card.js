9f36eaf465aeeaa7d7cf41efa2331ca0e68b6bd4
Tijana Zajic <tijanazajic1204@gmail.com>
2023-04-01 17:29:03 +0200

    Card style

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,9 +9,7 @@ const Card = ({candidates}) => {
         <div className='container row' id='candidate'>
             {candidates.map(candidate => (
                 <div className='card col' onClick={() => navigate(`/candidate/${candidate.id}`)}>
-                <img src= {
-                        candidate.avatar
-                    } alt='candidate' id='candidate-avatar'/>
+                <img src= {candidate.avatar} alt='candidate' id='candidate-avatar'/>
             <div className='card-name'> {candidate.name}</div>
             <div className='card-email'> {candidate.email}</div>
                 </div>
